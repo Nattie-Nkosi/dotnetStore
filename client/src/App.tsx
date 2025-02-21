412270a8d53f050a71b1cfb22fbf1128d2595de3
Nattie-Nkosi <nkosin361@gmail.com>
2025-02-21 12:45:23 +0200

    Add CORS support, update Vite configuration, and implement product fetching in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,37 @@
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    price: 100,
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    price: 200,
-  },
-];
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState<{ name: string; price: number }[]>(
+    []
+  );
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const addProduct = async () => {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: "Product 1" + (prevState.length + 1),
+        price: (prevState.length + 1) * 100,
+      },
+    ]);
+  };
+
   return (
     <>
       <h1>App</h1>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li key={product.name}>
             {product.name} - {product.price}
           </li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add</button>
     </>
   );
 }
